da6633304c9fb44fdc2442138289b76aee174bd0
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-30 17:37:52 -0300

    listen port for heroku var and 80 default

--- index.js
+++ index.js
@@ -20,10 +20,12 @@
 const server = require("./src/app.js");
 const { conn, User, Transaction } = require("./src/db.js");
 
+const port = process.env.PORT || 80;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
   /*
   await User.create({ username: "usern1" });
