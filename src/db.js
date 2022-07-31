4cdf9598a73edf05c276c8a3ab0777f450f58bbc
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-30 21:41:58 -0300

    ssl dialectoptions to true of new Sequelize

--- src/db.js
+++ src/db.js
@@ -8,6 +8,10 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,
   native: false,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true,
+  },
 });
 const basename = path.basename(__filename);
 
