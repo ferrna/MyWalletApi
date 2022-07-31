154e0022bbe7de100619af5764a7615e03ff4dcd
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-30 23:11:20 -0300

    welcome route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const path = require('path');
 const usersRouter = require("./user/users.router.js");
 const transactionsRouter = require("./transactions/transactions.router.js");
 
 /* GET home page. */
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 router.use("/user", usersRouter);
 router.use("/transactions", transactionsRouter);
 
