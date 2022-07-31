c0046be2d826abd48016214397f46e7bcbac2502
Fernando Arriondo <arriondovfernando@gmail.com>
2022-07-30 21:54:13 -0300

    sd

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 //${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}
 //postgres:audinac747@localhost:5432/challengealkemyfs
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@ec2-100-26-39-41.compute-1.amazonaws.com/dbadq5pojigai9`,
   {
     logging: false,
-    dialect: "postgres",
+    dialect: 'postgres',
     native: true,
     ssl: true,
     dialectOptions: {
@@ -21,10 +21,10 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-fs.readdirSync(path.join(__dirname, "/models"))
-  .filter((file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js")
+fs.readdirSync(path.join(__dirname, '/models'))
+  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 modelDefiners.forEach((model) => model(sequelize));
@@ -36,11 +36,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { User, Transaction } = sequelize.models;
 
 User.hasMany(Transaction, {
-  foreignKey: "id_user",
+  foreignKey: 'id_user',
   timestamps: false,
 });
 Transaction.belongsTo(User, {
-  foreignKey: "id_user",
+  foreignKey: 'id_user',
   timestamps: false,
 });
 
